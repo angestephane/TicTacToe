a9210de8f941526f05aa3bf0f829a10811d967c5
stephane boguhe <boguhestephane97@gmail.com>
2021-03-06 11:12:31 +0100

    Enum Ajouer.java
    Cette énumération contient toutes  des constantes. Qui sont les éléments que peuvent contenir une cellule

---
+++ tictactoe/AJouer.java
@@ -0,0 +1,14 @@
+/*
+ * Nous definissons une enumeration pour definir le contenu de notre cellule. 
+ * Une cellule peut contenir une et une seule valeur. Qui peut être soit
+ * le vide " ", un X ou un O
+ */
+package stephane.tictactoe;
+
+/**
+ *
+ * @author Ange Stephane
+ */
+public enum AJouer {
+    VIDE, ROND, CROIX
+}
