1ca932f2a07ce1c8048cfff5efb08e3d127aca30
stephane boguhe <boguhestephane97@gmail.com>
2021-03-06 11:43:49 +0100

    Enum EtatJeu.java
    Définir les constantes, représentant l'état du jeu. Soit on un jeu en cours, soit match nul soit victoire du joueurs 1 soit victoire du joueurs 2

---
+++ tictactoe/EtatJeu.java
@@ -0,0 +1,14 @@
+/*
+ * Dans cette classe nous definisson les différents états de notre jeu
+ * Ces états étant donc des constantes.
+ * Les états du jeu sont sauvegarder dans ces constantes
+ */
+package stephane.tictactoe;
+
+/**
+ *
+ * @author Ange Stephane
+ */
+public enum EtatJeu {
+    JOUER, NULLE, JOUEUR1_GAGNE, JOUEUR2_GAGNE
+}
